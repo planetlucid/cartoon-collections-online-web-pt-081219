ec29c1622dfe90f8361a58abad795e40622f2b61
Ryan S <planetlucid@gmail.com>
2019-08-24 20:48:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+roll_call_dwarves(dwarves)
+
+#
+#
+#
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! { |summon| summon.capitalize + '!' } 
+end
+summon_captain_planet(planeteer_calls)
+
+#
+#
+#
+
+def long_planeteer_calls (calls)
+    calls.any? {|word| word.length  > 4} 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+long_planeteer_calls(planeteer_calls)
+#
+#
+#
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
